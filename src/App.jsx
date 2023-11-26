a3dd8a6462b7419f958ce0db6838a949678694c1
rahulbabu7 <rahulbabu436@gmail.com>
2023-11-27 00:55:47 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,81 @@
-import'./App.css';
-import { useState } from 'react';
+import "./App.css";
+// import { useState } from 'react';
 
-const App =  ()=> {
-  // const name = "Rahul";
+const Person = (props) => {
+  return <>
+   <h1>
+     First Name : {props.name}
+   </h1>
+   <h1>
+    Last Name : {props.lname}
+   </h1>
+   <h1>
+    Age : {props.age}
+   </h1>
+   
+  
+  
+  </>;
+};
+
+const App = () => {
+  const name = "Rahul";
   // const  isLoggedIn = true;
-  const [counter ,setCounter ] = useState(0);
+  // const [counter ,setCounter ] = useState(0);
   return (
     <>
       <div>
         {/* <h1>Hello {isLoggedIn ? name: "Hii"}</h1> */}
         <main>
           <section>
-            <h2>
+            {
+              //  curly braces is  used to write javascript here
+              name ? (
+                <>
+                  <h1> {name}</h1>
+                </>
+              ) : (
+                // <h1>
+                //   Hello
+                // </h1>
+                // <h2></h2>      Adjacent jsx elements without <>  </>  causes errors
+                <>
+                  <h2>Hello</h2>
+                  <h1>Hiii</h1>
+                </>
+              )
+
+              //! Explanation of this braces
+              // if there is a value for name then print test
+              //else print the h1 and h2
+              //<.>  </>  this means an empty div
+            }
+
+            {/* <h2>
               Counter
             </h2>
             <button onClick={()=> setCounter()}> + </button>
             <h1>
               {counter}
             </h1>
-            <button> - </button>
+            <button> - </button> */}
           </section>
+          <Person 
+          name = {'Rahul'}
+          lname = {"Babu"} 
+          age = {20}
+          />
+         
+         <Person 
+          name = {'suii'}
+          lname = {"hehe"} 
+          age = {10}
+          />
+
         </main>
       </div>
     </>
   );
-}
+};
 
 export default App;
